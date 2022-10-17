f96229e89b4f7327a69f2632e3267cb2106ef1ab
Luis <inkedco@gmail.com>
2022-10-17 01:36:46 -0300

    primeras lineas de js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hola mundo");
+// Algoritmo para levantar pedido segun barrio donde vive
+let nombreCliente = prompt("Por favor indique su nombre");
+let telefonoCliente = prompt("Por favor indique su número de teléfono")
+let direCliente = prompt("indique su direccion")
+let pedidoCliente = nombreCliente + telefonoCliente + direCliente;
+console.log("Hola "+ nombreCliente +  " su telefono es" + telefonoCliente +  " y su barrio donde le llevaremmos su pedidos es" + direCliente);
+
+
