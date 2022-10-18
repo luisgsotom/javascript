be5f466c511e1b3b137957f9ba040bf0cc254b6c
Luis <inkedco@gmail.com>
2022-10-18 01:59:48 -0300

    a

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 //Algoritmo para solicitar datos a cliente y direccion para delivery
-let nombre = prompt("Por favor ingrese su nomnbre")
-let telefono = parseInt(prompt("Por favor ingrese su numero de telefono"))
-let direccion = prompt("Si vivis en San Nicolas, Balvanera o Recoleta. Podemos hacer el Delivery")
+let bandera = true
+do {
+    let nombre = prompt("Por favor ingrese su nomnbre")
+    let telefono = parseInt(prompt("Por favor ingrese su numero de telefono"))
+    let direccion = prompt("Si vivis en San Nicolas, Balvanera o Recoleta.  Podemos hacer el Delivery")
 
-let barrio = direccion
-if (barrio =="san nicolas"){
-    console.log(`Hola ${nombre} tu numero es ${telefono} y tenemos delivery a ${barrio}`)
-}
-else if (barrio =="balvanera"){
-    console.log(`Hola ${nombre} tu numero es ${telefono} y tenemos delivery a ${barrio}`)
-}
-else if (barrio =="recoleta"){
-    console.log(`Hola ${nombre} tu numero es ${telefono} y tenemos delivery a ${barrio}`)
-}
-else {
-    console.log(`Ups! Perdon ${nombre} estas fuera de rango de Delivery`)
-}
+    let barrio = direccion
+    if (barrio.toLowerCase() =="san nicolas"){
+        console.log(`Hola ${nombre} tu numero es ${telefono} y tenemos  delivery a ${barrio}`)
+    }
+    else if (barrio.toLowerCase() =="balvanera"){
+        console.log(`Hola ${nombre} tu numero es ${telefono} y tenemos  delivery a ${barrio}`)
+    }
+    else if (barrio.toLowerCase() =="recoleta"){
+        console.log(`Hola ${nombre} tu numero es ${telefono} y tenemos  delivery a ${barrio}`)
+    }
+    else {
+        console.log(`Ups! Perdon ${nombre} estas fuera de rango de Delivery`)
+    }
+    let preguntar = prompt (`Deseas volver a ingresar tus datos?
+    "NO" para salir`)
+    if (preguntar.toUpperCase() == "no"){
+        bandera = false
+        alert("Gracias por completar tus datos")
+    }
+}while(bandera)
