1e49463f8f85b39b761877fae18b350de0d23fef
Luis <inkedco@gmail.com>
2022-10-18 00:52:12 -0300

    a

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function nombreCliente(){
-    let cliente = prompt("Por favor ingrese su nombre")
-    return cliente
+let nombre = prompt("Por favor ingrese su nomnbre")
+let telefono = parseInt(prompt("Por favor ingrese su numero de telefono"))
+let direccion = prompt("Si vivis en San Nicolas, Balvanera o Recoleta. Podemos hacer el Delivery")
+
+let barrio = direccion
+if (barrio =="san nicolas"){
+    console.log(`Tenemos delivery a ${barrio}`)
+}
+else if (barrio =="balvanera"){
+    console.log(`Tenemos delivery a ${barrio}`)
+}
+else if (barrio =="recoleta"){
+    console.log(`Tenemos delivery a ${barrio}`)
 }
