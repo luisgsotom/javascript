c34a62b66954159e225bc475a8bf3c4d9c751639
Luis <inkedco@gmail.com>
2022-10-17 23:58:42 -0300

    tarea coder js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// Algoritmo para levantar pedido segun barrio donde vive
-let nombreCliente = prompt("Por favor indique su nombre");
-let telefonoCliente = prompt("Por favor indique su número de teléfono")
-let direCliente = prompt("indique su direccion")
-let pedidoCliente = nombreCliente + telefonoCliente + direCliente;
-console.log("Hola "+ nombreCliente +  " su telefono es" + telefonoCliente +  " y su barrio donde le llevaremmos su pedidos es" + direCliente);
-
-
+function nombreCliente(){
+    let cliente = prompt("Por favor ingrese su nombre")
+    return cliente
+}
